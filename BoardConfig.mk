dc13b6f1e4fc2359f92efbb8a669c89ab8000eb8
John Dias <joaodias@google.com>
2017-06-21 13:14:52 -0700

    whyred: Pre-opt SystemUI
    SystemUI is getting deoptimized, possibly resulting in jank
    (not yet validated); pre-optimize to avoid deoptimization for now.
    Bug: 62832717
    Bug: 35872915
    Test: boot, check for vdex, run for a bit
    
    Change-Id: Ia754db444eebaebf0ff2389ab66f6f1d486da994
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Nidhun Balaji T R <nidhunbalaji@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -276,6 +276,7 @@ ifeq ($(HOST_OS),linux)
       WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
   endif
 endif
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
 # inherit from the proprietary version
 -include vendor/xiaomi/whyred/BoardConfigVendor.mk
