531fe2dae971eafafefec80327d0dc471db8355b
Offain <fabian.leutenegger@bluewin.ch>
2019-01-07 19:25:03 +0530

    whyred: build caf power HIDL HAL
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>
    Signed-off-by: Nidhun Balaji T R <nidhunbalaji@gmail.com>

--- device.mk
+++ device.mk
@@ -320,7 +320,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    power.qcom \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
