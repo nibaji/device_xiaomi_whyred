0bf0e5f95f9f77191ca97a37a29eb39a02c5fedb
celtare21 <celtare21@gmail.com>
2019-01-24 11:21:07 +0000

    BoardConfig: Properly set dexpreopt
    Change-Id: Icb2e129000676938b75b94c1279b8df676a4ed21
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Nidhun Balaji T R <nidhunbalaji@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -268,13 +268,15 @@ WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
-# Enable dex pre-opt to speed up initial boot
+# Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_DEBUG_INFO := false
+      USE_DEX2OAT_DEBUG := false
+      DONT_DEXPREOPT_PREBUILTS := true
+      WITH_DEXPREOPT_PIC := true
       WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
   endif
 endif
 
